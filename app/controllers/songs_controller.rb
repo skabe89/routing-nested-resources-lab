63e87cde13d3899935ef10f909fb5a668a9c388a
Sean <skabe89@gmail.com>
2021-01-24 12:20:39 -0500

    commit tree 685a4be2a8a4de9fe9e2eef71e30daad66f5aaaa

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,27 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      artist = Artist.find_by(id: params[:artist_id])
+        if artist
+          @songs = artist.songs
+        else
+          redirect_to artists_path, alert: "Artist not found"
+        end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      @artist= Artist.find_by(id: params[:artist_id])
+      @song = @artist.songs.find_by(id: params[:id])
+        if @song.nil?
+          redirect_to artist_songs_path(@artist), alert: "Song not found"
+        end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def new
@@ -49,5 +66,10 @@ class SongsController < ApplicationController
   def song_params
     params.require(:song).permit(:title, :artist_name)
   end
+
+  def redirect_to_artist_if_song_not_found
+    redirect_to artist_songs_path unless @song
+  end
+
 end
 
